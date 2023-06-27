65b72ee1ea7bd693a3619551d1a2a25651b8d8f5
ouyanghao <ouyanghaobbb@foxmail.com>
2023-06-27 23:51:59 +0800

    feat: 尺子功能开发

--- src/views/Edit/Main/index.tsx
+++ src/views/Edit/Main/index.tsx
@@ -3,22 +3,35 @@ import styles from "./index.module.scss";
 
 const App: FC = () => {
   const wrapper = useRef<HTMLDivElement | null>(null);
+  const ruleX = useRef<HTMLCanvasElement | null>(null);
 
   useEffect(() => {
-    if (!wrapper.current) return;
-    let [mainWidth, mainHeight] = [0, 0];
+    if (!wrapper.current || !ruleX.current) return;
     const info = wrapper.current.getBoundingClientRect();
-    console.log(info.width, info.height, 333);
     const { width, height } = info;
-    mainWidth = width;
-    mainHeight = height;
-  }, [wrapper]);
+    ruleX.current.width = width;
+    ruleX.current.height = 24;
+
+    const ctx = ruleX.current.getContext("2d");
+    if (!ctx) return;
+    ctx.clearRect(0, 0, width, height);
+    ctx.beginPath();
+    ctx.lineWidth = 0.5;
+    ctx.strokeStyle = "#fff";
+    ctx.moveTo(24, 0);
+    ctx.lineTo(24, 16);
+    ctx.stroke();
+  }, [wrapper, ruleX]);
 
   return (
     <div ref={wrapper} className={`h-full w-full ${styles["main-box"]}`}>
       <div className={styles["main-header"]}></div>
       <div className={styles["content-box"]}>
-        <canvas className={styles["rule-x"]} />
+        <canvas
+          ref={ruleX}
+          className={styles["rule-x"]}
+          style={{ border: "1px solid #ff0000" }}
+        />
         <canvas className={styles["rule-y"]} />
       </div>
     </div>

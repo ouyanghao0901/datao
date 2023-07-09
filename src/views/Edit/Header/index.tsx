b26b400b5f943dde82119b28005c5ecac55e328c
ouyanghao <ouyanghaobbb@foxmail.com>
2023-07-09 12:37:28 +0800

    feat: 页面布局开发

--- src/views/Edit/Header/index.tsx
+++ src/views/Edit/Header/index.tsx
@@ -2,7 +2,15 @@ import { FC } from "react";
 import styles from "./index.module.scss";
 
 const App: FC = () => {
-  return <div className={styles.container}>{/* unitLength */}</div>;
+  return (
+    <div className={`${styles.container} f-x`}>
+      <div className={"f-1 f-x a-i-c"}>左</div>
+      <div className={"f-1 f-x j-c-c a-i-c"}>
+        <div className={styles["switch-box"]}></div>
+      </div>
+      <div className={"f-1 f-x j-c-fe a-i-c"}>右</div>
+    </div>
+  );
 };
 
 export default App;

f4e1c0318903afcd1ee55f2a0a8699a28e8159c1
ouyanghao <ouyanghaobbb@foxmail.com>
2023-07-10 21:56:24 +0800

    feat: 页面布局开发

--- src/views/Edit/Header/index.tsx
+++ src/views/Edit/Header/index.tsx
@@ -1,14 +1,53 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import styles from "./index.module.scss";
 
 const App: FC = () => {
+  const [current, setCurrent] = useState("pc");
+  const [rightMenu] = useState([
+    { label: "全部设置", icon: "icon-shezhi-xianxing" },
+    { label: "预览", icon: "icon-yulan1" },
+    { label: "发布", icon: "icon-icon_fabu" },
+  ]);
+  const handleClick = () => {
+    setCurrent(current === "pc" ? "phone" : "pc");
+  };
   return (
     <div className={`${styles.container} f-x`}>
-      <div className={"f-1 f-x a-i-c"}>左</div>
-      <div className={"f-1 f-x j-c-c a-i-c"}>
-        <div className={styles["switch-box"]}></div>
+      <div className={"f-1 f-x a-i-c"}>
+        <div className={`f-x j-c-c a-i-c ${styles["left-menu-item"]}`}>
+          <i className={`iconfont icon-023tuceng `}></i>
+        </div>
+        <div className={`f-x j-c-c a-i-c ${styles["left-menu-item"]}`}>
+          <i className="iconfont icon-layout-two"></i>
+        </div>
       </div>
-      <div className={"f-1 f-x j-c-fe a-i-c"}>右</div>
+      <div className={"f-1 f-x j-c-c a-i-c"}>
+        <div className={styles["switch-box"]} onClick={handleClick}>
+          <div className={styles["switch-item"]}>
+            <i className="iconfont icon-PCtaishiji"></i>
+            <i className="iconfont icon-shumashouji"></i>
+          </div>
+          <div
+            className={`${styles["slid"]} ${
+              current === "phone" ? styles["current"] : ""
+            }`}
+          ></div>
+        </div>
+        <div className={styles["title"]}>test</div>
+      </div>
+      <div className={`f-1 f-x j-c-fe a-i-c ${styles["right-menu"]}`}>
+        {rightMenu.map(i => {
+          return (
+            <div
+              key={i.icon}
+              className={`f-y a-i-c ${styles["right-menu-item"]}`}
+            >
+              <i className={`iconfont ${i.icon}`}></i>
+              <span>{i.label}</span>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
--- src/views/Edit/LeftPanel/index.tsx
+++ src/views/Edit/LeftPanel/index.tsx
@@ -1,22 +1,32 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 
 import styles from "./index.module.scss";
 
 const App: FC = () => {
+  const [list] = useState([
+    "icon-upper",
+    "icon-lower",
+    "icon-toTop",
+    "icon-toBottom",
+  ]);
   return (
     <div className={styles["left-box"]}>
-      {/* <div className={styles["left-header"]}>图层</div> */}
+      <div className={styles["left-header"]}>图层</div>
       <div className={styles["left-operator"]}>
-        <div className={styles["el-tooltip__trigger"]}></div>
+        <div className={styles["el-tooltip__trigger"]}>
+          {list.map(i => {
+            return <i className={`iconfont ${i}`} key={i}></i>;
+          })}
+        </div>
       </div>
       <div className={styles["el-scrollbar"]}>
-        {/* <div className={styles.node}>节点名称</div>
         <div className={styles.node}>节点名称</div>
         <div className={styles.node}>节点名称</div>
         <div className={styles.node}>节点名称</div>
         <div className={styles.node}>节点名称</div>
         <div className={styles.node}>节点名称</div>
-        <div className={styles.node}>节点名称</div> */}
+        <div className={styles.node}>节点名称</div>
+        <div className={styles.node}>节点名称</div>
       </div>
     </div>
   );
